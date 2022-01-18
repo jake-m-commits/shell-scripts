bc050957b87dc19c6b94cf9eccebb00cfd39d3c0
Jake Martin <jm9357481@gmail.com>
2022-01-18 11:37:15 -0800

    updates get_info.sh with printf and colors

--- get_info.sh
+++ get_info.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 #: report with date and system components(cpu model, total mem, disk space)
 
-echo Log Date: $(date) && echo
-echo Home directory:
-echo $HOME
-ls -a ~/ && echo
-echo CPU and MEM information:
+G='\033[0;32m'
+C='\033[0m'
+
+printf "${G}Log Date:${C} $(date)\n\n"
+printf "${G}Home directory ($HOME):${C}\n"
+ls -a $HOME && echo
+printf "${G}CPU and MEM information:${C}\n"
 cat /proc/cpuinfo | grep "model name" | head -n 1
 cat /proc/cpuinfo | grep "cpu cores" | head -n 1
 cat /proc/cpuinfo | grep "cpu MHz" | head -n 1
 cat /proc/meminfo | grep "MemTotal"
 cat /proc/meminfo | grep "SwapTotal" && echo
-echo Disk partition information:
+printf "${G}Disk partition information:${C}\n"
 df -h --output=source,fstype,size,used,avail,pcent,target -x tmpfs -x devtmpfs
 
 #END
